68a309fc325107d5aec94009bc8577c0ee7af6c3
Kelvin-EASJ <kelvin.aggerholm@gmail.com>
2020-05-19 13:35:09 +0200

    Tilføjet Login Activity og intent

--- 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/View/LoginActivity.java
+++ 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/View/LoginActivity.java
@@ -1,14 +1,16 @@
-package com.example.android.androidapp;
+package com.example.android.androidapp.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.android.androidapp.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
