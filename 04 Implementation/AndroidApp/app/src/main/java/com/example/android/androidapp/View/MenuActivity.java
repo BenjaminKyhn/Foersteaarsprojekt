d00f0d1f5c243b5994f94a24454aac92edd3d3e2
Kelvin-EASJ <kelvin.aggerholm@gmail.com>
2020-05-20 11:00:41 +0200

    Tilføjet MenuActivity

---
+++ 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/View/MenuActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.androidapp.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.android.androidapp.R;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
