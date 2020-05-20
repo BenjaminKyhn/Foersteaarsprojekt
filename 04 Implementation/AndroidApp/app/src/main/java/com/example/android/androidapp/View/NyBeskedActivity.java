7a6a7af203d67cd104d10c80707acf4fc1e6b0d1
Patrick <patr6996@edu.easj.dk>
2020-05-20 12:28:59 +0200

    NyBesked View

---
+++ 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/View/NyBeskedActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.androidapp.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.android.androidapp.R;
+
+public class NyBeskedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ny_besked);
+    }
+}
