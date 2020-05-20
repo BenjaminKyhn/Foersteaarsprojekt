906cd3ca546d022fb1c2537963051a6887958813
Benjamin Kyhn <benkyhn@hotmail.com>
2020-05-20 18:31:57 +0200

    getInstance()-metode tilføjet i DatabaseManager.java for at undgå problemet med flere instanser af DatabaseManager

--- 04 Implementation/DesktopApp/src/main/java/view/Start.java
+++ 04 Implementation/DesktopApp/src/main/java/view/Start.java
@@ -5,8 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import model.BrugerFacade;
-import persistence.DatabaseManager;
 
 /**
  * @author Benjamin & Tommy
