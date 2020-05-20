3913b8d951243919a43ce4b600365e3dd845b263
Tommy Gultiano Hansen <54972963+EWCMF@users.noreply.github.com>
2020-05-20 13:26:18 +0200

    Implementeret SD0X01 hentBrugerMedMail.

--- 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/persistence/DatabaseManager.java
+++ 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/persistence/DatabaseManager.java
@@ -1,14 +1,33 @@
 package com.example.android.androidapp.persistence;
 
+import androidx.annotation.NonNull;
+
 import com.example.android.androidapp.domain.Bruger;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 /** @author Tommy **/
 public class DatabaseManager {
+    FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+
     public void gemBruger(Bruger bruger) {
 
     }
 
     public Bruger hentBrugerMedEmail(String email) {
-        return null;
+        final Bruger[] bruger = {null};
+        firestore.collection("brugere").document(email).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                DocumentSnapshot document = task.getResult();
+                assert document != null;
+                if (document.exists()) {
+                    bruger[0] = document.toObject(Bruger.class);
+                }
+            }
+        });
+        return bruger[0];
     }
 }
