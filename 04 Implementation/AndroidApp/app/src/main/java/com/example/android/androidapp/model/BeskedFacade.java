52f767f32ee13b5194c6ce72b51205fa6d4a307c
Tommy Gultiano Hansen <54972963+EWCMF@users.noreply.github.com>
2020-05-21 16:24:58 +0200

    Implementering af UC03 Send Beskeder (WIP).

--- 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/model/BeskedFacade.java
+++ 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/model/BeskedFacade.java
@@ -1,10 +1,14 @@
 package com.example.android.androidapp.model;
 
 import com.example.android.androidapp.domain.Chat;
+import com.example.android.androidapp.model.exceptions.ForMangeTegnException;
+import com.example.android.androidapp.model.exceptions.TomBeskedException;
+import com.example.android.androidapp.model.exceptions.TomEmneException;
 
 /** @author Tommy **/
 public class BeskedFacade {
-    private BeskedManager beskedManager;
+    private BeskedManager beskedManager = new BeskedManager();
+    private Validering validering = new Validering();
 
     public Chat hentChat(String afsender, String modtager, String emne) {
         return beskedManager.hentChat(afsender, modtager, emne);
@@ -12,5 +16,13 @@ public class BeskedFacade {
 
     public void sendBesked(String besked, Chat chat) {
         beskedManager.sendBesked(besked, chat);
+    }
+
+    public void tjekEmne(String emne) throws TomEmneException, ForMangeTegnException {
+        validering.tjekEmne(emne);
+    }
+
+    public void tjekBesked(String besked) throws TomBeskedException, ForMangeTegnException {
+        validering.tjekBesked(besked);
     }
 }
