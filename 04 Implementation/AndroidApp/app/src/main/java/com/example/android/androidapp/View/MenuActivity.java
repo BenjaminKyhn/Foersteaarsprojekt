25ef63890317852e8efe5a481abd3b4e5aa6f648
Patrick <patr6996@edu.easj.dk>
2020-05-21 11:02:34 +0200

    ConfiguredNextButton, in Java - Activities. activity_login tilføjet activity_nybesked tilføjet

--- 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/View/MenuActivity.java
+++ 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/View/MenuActivity.java
@@ -2,7 +2,10 @@ package com.example.android.androidapp.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.android.androidapp.R;
 
@@ -12,5 +15,15 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        configureIndbakkeButton();
+    }
+    private void configureIndbakkeButton() {
+        Button changeActivityButton = findViewById(R.id.BeskederButton);
+        changeActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MenuActivity.this, indbakkeActivity.class));
+            }
+        });
     }
 }
--- 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/view/LoginActivity.java
+++ 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/view/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.android.androidapp.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.android.androidapp.R;
 
@@ -12,5 +15,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        configureLogindButton();
+    }
+    private void configureLogindButton() {
+        Button changeActivityButton = findViewById(R.id.logindButton);
+        changeActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, MenuActivity.class));
+            }
+        });
     }
 }
