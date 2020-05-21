3f0e8425e5455fac5ae3abb9a4a18ac1ffa3ca14
Patrick <patr6996@edu.easj.dk>
2020-05-21 13:23:05 +0200

    LaesBeskedActivity (besvarButton) --> scene skift til BesvarBeskedActivity implementeret.

--- 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/View/LaesBeskedActivity.java
+++ 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/View/LaesBeskedActivity.java
@@ -2,7 +2,10 @@ package com.example.android.androidapp.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.android.androidapp.R;
 
@@ -12,5 +15,16 @@ public class LaesBeskedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_laes_besked);
+        configureBesvarBeskedButton();
+    }
+
+    private void configureBesvarBeskedButton() {
+        TextView changeActivityButton = findViewById(R.id.besvarButton);
+        changeActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LaesBeskedActivity.this, BesvarBeskedActivity.class));
+            }
+        });
     }
 }
