63180fe455fd70af3f9a08a27bce1f35a18c5fff
Patrick <patr6996@edu.easj.dk>
2020-05-21 13:03:41 +0200

    configureButton = indlaesBesked

--- 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/View/indbakkeActivity.java
+++ 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/View/indbakkeActivity.java
@@ -2,7 +2,10 @@ package com.example.android.androidapp.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.android.androidapp.R;
 
@@ -12,5 +15,36 @@ public class indbakkeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_indbakke);
+        configureLaesBeskedButton();
+    }
+    private void configureLaesBeskedButton() {
+        TextView changeActivityButton1 = findViewById(R.id.beskedEmne1);
+        changeActivityButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(indbakkeActivity.this, LaesBeskedActivity.class));
+            }
+        });
+        TextView changeActivityButton2 = findViewById(R.id.besked1);
+        changeActivityButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(indbakkeActivity.this, LaesBeskedActivity.class));
+            }
+        });
+        TextView changeActivityButton3 = findViewById(R.id.beskedEmne2);
+        changeActivityButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(indbakkeActivity.this, LaesBeskedActivity.class));
+            }
+        });
+        TextView changeActivityButton4 = findViewById(R.id.besked2);
+        changeActivityButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(indbakkeActivity.this, LaesBeskedActivity.class));
+            }
+        });
     }
 }
