aaa4759bf3228b26012ad192e757ee911abb3320
Patrick <patr6996@edu.easj.dk>
2020-05-22 11:12:48 +0200

    BurgerMenu Added everywhere

--- 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/View/MainActivity.java
+++ 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/View/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.android.androidapp.R;
 
@@ -33,6 +34,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, NyBeskedActivity.class));
+            }
+        });
+    }
+    private void configureBurgerButton() {
+        ImageView changeActivityButton = findViewById(R.id.burgerMenu);
+        changeActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, NaviView.class));
             }
         });
     }
