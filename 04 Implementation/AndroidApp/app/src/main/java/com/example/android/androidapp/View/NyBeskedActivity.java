176258d8beffd7a1c2ea34346db1fe765ee30813
Tommy Gultiano Hansen <54972963+EWCMF@users.noreply.github.com>
2020-05-22 12:49:06 +0200

    Quick fix af fejl i UI efter merge.

--- 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/View/NyBeskedActivity.java
+++ 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/View/NyBeskedActivity.java
@@ -6,14 +6,6 @@ import android.os.Bundle;
 import com.example.android.androidapp.R;
 
 public class NyBeskedActivity extends AppCompatActivity {
-	
-	
-import android.view.View;
-
-
-
-public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/view/LoginActivity.java
+++ 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/view/LoginActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
 import com.example.android.androidapp.R;
 
@@ -17,7 +16,6 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         configureLogindButton();
-        configureBurgerButton();
     }
     private void configureLogindButton() {
         Button changeActivityButton = findViewById(R.id.logindButton);
@@ -25,15 +23,6 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(LoginActivity.this, MenuActivity.class));
-            }
-        });
-    }
-    private void configureBurgerButton() {
-        ImageView changeActivityButton = findViewById(R.id.burgerMenu);
-        changeActivityButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(LoginActivity.this, NaviView.class));
             }
         });
     }
