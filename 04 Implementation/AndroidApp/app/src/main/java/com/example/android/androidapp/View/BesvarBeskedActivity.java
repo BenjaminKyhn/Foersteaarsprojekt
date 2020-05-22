850da5f4687573fd3b9d2c309f83945677764f94
Patrick <patr6996@edu.easj.dk>
2020-05-22 14:44:24 +0200

    Skabte configuresendButton( BesvarBeskedActivity --> SendBeskedActivity )

--- 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/View/BesvarBeskedActivity.java
+++ 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/View/BesvarBeskedActivity.java
@@ -2,7 +2,11 @@ package com.example.android.androidapp.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.android.androidapp.R;
 
@@ -12,6 +16,15 @@ public class BesvarBeskedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_besvar_besked);
+        configuresendButton();
     }
-
+    private void configuresendButton() {
+        Button changeActivityButton = findViewById(R.id.sendButton);
+        changeActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(BesvarBeskedActivity.this, SendBeskedActivity.class));
+            }
+        });
+    }
 }
