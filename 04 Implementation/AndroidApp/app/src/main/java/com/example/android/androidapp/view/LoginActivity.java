482dd4b9c71a3a0ad3cb9d17b196b53aae96c769
Tommy Gultiano Hansen <54972963+EWCMF@users.noreply.github.com>
2020-05-24 20:54:24 +0200

    Tilføjede flere versioner af layout.

--- 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/view/LoginActivity.java
+++ 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/view/LoginActivity.java
@@ -7,18 +7,30 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.android.androidapp.R;
+import com.example.android.androidapp.model.BrugerFacade;
+
 /**@author Patrick**/
 public class LoginActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
+    EditText editTextEmail;
+    EditText editTextPassword;
+    BrugerFacade brugerFacade;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         drawerLayout = findViewById(R.id.drawer_layout);
+
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+
+        brugerFacade = BrugerFacade.hentInstans();
 
         ImageView menu = findViewById(R.id.burgerMenu);
 
@@ -44,7 +56,15 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-    public void skiftTilMenu(View view) {
-        startActivity(new Intent(this, MenuActivity.class));
+    public void logInd(View view) {
+        String email = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString();
+        boolean loggedeInd = brugerFacade.logInd(email, password);
+        if (loggedeInd) {
+            startActivity(new Intent(this, MenuActivity.class));
+        }
+        else {
+            Toast.makeText(this, "Login fejlede", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/view/OpretBrugerActivity.java
+++ 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/view/OpretBrugerActivity.java
@@ -6,19 +6,35 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.androidapp.R;
+import com.example.android.androidapp.model.BrugerFacade;
+import com.example.android.androidapp.model.exceptions.BrugerLoggedeIndException;
+
 /**@author Kelvin**/
 public class OpretBrugerActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
+    BrugerFacade brugerFacade;
+    EditText navnInput;
+    EditText emailInput;
+    EditText passwordInput;
+    EditText gentagPasswordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opret_bruger);
         drawerLayout = findViewById(R.id.drawer_layout);
+        brugerFacade = BrugerFacade.hentInstans();
+
+        navnInput = findViewById(R.id.navnFelt);
+        emailInput = findViewById(R.id.emailFelt);
+        passwordInput = findViewById(R.id.passwordFelt);
+        gentagPasswordInput = findViewById(R.id.gentagPasswordFelt);
 
         ImageView menu = findViewById(R.id.burgerMenu);
 
@@ -35,6 +51,23 @@ public class OpretBrugerActivity extends AppCompatActivity {
 
     public void openDrawer() {
         drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    public void opretBruger(View view) {
+        String navn = navnInput.getText().toString();
+        String email = emailInput.getText().toString();
+        String password = passwordInput.getText().toString();
+        String gentagPassword = passwordInput.getText().toString();
+
+        if (!password.equals(gentagPassword)) {
+            Toast.makeText(this, "Password'et blev ikke tastede korrekt", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        try {
+            brugerFacade.opretBruger(navn, email, password);
+        } catch (BrugerLoggedeIndException e) {
+            Toast.makeText(this, "Bruger må ikke være logged ind.", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
