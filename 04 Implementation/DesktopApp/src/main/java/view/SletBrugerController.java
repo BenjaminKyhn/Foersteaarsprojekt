bcc611e90bd86535e1b89055ce2f2bb540832285
Benjamin Kyhn <benkyhn@hotmail.com>
2020-05-25 16:02:49 +0200

    SletBruger implementeret

--- 04 Implementation/DesktopApp/src/main/java/view/SletBrugerController.java
+++ 04 Implementation/DesktopApp/src/main/java/view/SletBrugerController.java
@@ -1,25 +1,65 @@
 package view;
 
+import domain.Bruger;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import model.BrugerFacade;
+import model.exceptions.*;
 
+/** @author Benjamin */
 public class SletBrugerController {
     private BrugerFacade brugerFacade;
+    private Bruger aktivBruger;
+
+    @FXML
+    private AnchorPane sletBrugerAnchorPane;
 
     @FXML
     private Button btnSletBruger;
 
     @FXML
-    private TextField tfNavn, tfEmail, tfPassword;
+    private PasswordField tfPassword, tfGentagPw;
 
     public void initialize() {
         brugerFacade = BrugerFacade.getInstance();
-
+        aktivBruger = brugerFacade.getAktivBruger();
+        btnSletBruger.setOnMouseClicked(event -> sletBruger());
     }
 
-    public void sletBruger(){
-//        brugerFacade.sletBruger();
+    public void sletBruger() {
+        String password = tfPassword.getText();
+        String gentagPassword = tfGentagPw.getText();
+        if (!password.equals(gentagPassword))
+            System.out.println("Fejl: password matcher ikke");
+
+        try {
+            brugerFacade.sletBruger(aktivBruger, password);
+            skiftTilStartscene();
+        } catch (ForkertPasswordException fpe){
+            System.out.println("Fejl: passwordet er forkert");
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void skiftTilStartscene(){
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("../Start.fxml"));
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        assert root != null;
+        Scene secondScene = new Scene(root);
+
+        Stage stage = (Stage) sletBrugerAnchorPane.getScene().getWindow();
+        stage.setScene(secondScene);
     }
 }
