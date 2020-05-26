75a50193cb003754dcfc7bcf49a831cbc5cf52c6
Tommy Gultiano Hansen <54972963+EWCMF@users.noreply.github.com>
2020-05-26 23:51:28 +0200

    Simpel funktion tilføjede til navigations menuen. Man kan hoppe til beskeder og logge ud, hvis man er logged ind.

--- 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/view/NavigationHjaelper.java
+++ 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/view/NavigationHjaelper.java
@@ -1,8 +1,40 @@
 package com.example.android.androidapp.view;
 
-public class NavigationHjaelper {
+import android.content.Context;
+import android.content.Intent;
+import android.view.MenuItem;
 
-    public static void initialiserMenu() {
+import androidx.annotation.NonNull;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.example.android.androidapp.R;
+import com.example.android.androidapp.model.BrugerFacade;
+import com.google.android.material.navigation.NavigationView;
+
+class NavigationHjaelper {
+
+    static void initialiserMenu(final NavigationView navigationView, final DrawerLayout drawerLayout) {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (BrugerFacade.hentInstans().hentAktivBruger() != null) {
+                    Context context = navigationView.getContext();
+                    switch (item.getItemId()) {
+                        case R.id.indbakke:
+                            context.startActivity(new Intent(context, VaelgChatActivity.class));
+                            drawerLayout.closeDrawer(GravityCompat.START);
+                            break;
+                        case R.id.log_ud:
+                            context.startActivity(new Intent(context, MainActivity.class));
+                            drawerLayout.closeDrawer(GravityCompat.START);
+                            BrugerFacade.hentInstans().logUd();
+                            break;
+                    }
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
