c99d310a2cda480d246adbf76f0b832d4d8fcd79
Tommy Gultiano Hansen <54972963+EWCMF@users.noreply.github.com>
2020-05-27 08:43:52 +0200

    Navigation menu ændrer efter om man er loggede ind eller ej.

--- 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/view/NavigationHjaelper.java
+++ 04 Implementation/AndroidApp/app/src/main/java/com/example/android/androidapp/view/NavigationHjaelper.java
@@ -3,18 +3,36 @@ package com.example.android.androidapp.view;
 import android.content.Context;
 import android.content.Intent;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.android.androidapp.R;
+import com.example.android.androidapp.domain.Bruger;
 import com.example.android.androidapp.model.BrugerFacade;
 import com.google.android.material.navigation.NavigationView;
 
 class NavigationHjaelper {
 
     static void initialiserMenu(final NavigationView navigationView, final DrawerLayout drawerLayout) {
+        Bruger bruger = BrugerFacade.hentInstans().hentAktivBruger();
+        if (bruger == null) {
+            navigationView.removeHeaderView(navigationView.getHeaderView(0));
+            navigationView.inflateHeaderView(R.layout.navigation_header_gaest);
+            navigationView.getMenu().clear();
+            navigationView.inflateMenu(R.menu.drawer_menu_gaest);
+            return;
+        }
+        else {
+            TextView navn = navigationView.getHeaderView(0).findViewById(R.id.personNavn);
+            navn.setText(bruger.getNavn());
+            TextView mail = navigationView.getHeaderView(0).findViewById(R.id.personMail);
+            mail.setText(bruger.getEmail());
+        }
+        
+        navigationView.removeHeaderView(navigationView.findViewById(R.id.header));
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
