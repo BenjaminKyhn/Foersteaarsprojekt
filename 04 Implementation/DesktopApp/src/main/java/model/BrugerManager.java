ab1f8594c91ae6f6e9d30a0d654ae562422ba1e9
Benjamin Kyhn <benkyhn@hotmail.com>
2020-05-30 21:22:56 +0200

    TekstHasher-klasse oprettet

--- 04 Implementation/DesktopApp/src/main/java/model/BrugerManager.java
+++ 04 Implementation/DesktopApp/src/main/java/model/BrugerManager.java
@@ -3,11 +3,7 @@ package model;
 import domain.Bruger;
 import model.exceptions.BrugerLoggedIndException;
 import model.exceptions.ForkertPasswordException;
-import org.apache.commons.codec.binary.Hex;
 
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 /**
@@ -17,8 +13,10 @@ public class BrugerManager {
     private Bruger aktivBruger;
     private static BrugerManager brugerManager;
     private List<Bruger> brugere;
+    private TekstHasher tekstHasher;
 
     public BrugerManager() {
+        tekstHasher = new TekstHasher();
     }
 
     public static synchronized BrugerManager getInstance() {
@@ -33,35 +31,22 @@ public class BrugerManager {
             throw new BrugerLoggedIndException();
         }
 
-        String enkrypteretPassword = enkrypterTekst(password);
-        Bruger bruger = new Bruger(navn, email, enkrypteretPassword);
+        String hashedPassword = tekstHasher.hashTekst(password);
+        Bruger bruger = new Bruger(navn, email, hashedPassword);
         brugere.add(bruger);
     }
 
     public void sletBruger(Bruger bruger, String password) throws ForkertPasswordException {
-        String enkrypteretPassword = enkrypterTekst(password);
-        String enkrypteretBrugerPassword = bruger.getPassword();
+        String hashedPassword = tekstHasher.hashTekst(password);
+        String hashedBrugerPassword = bruger.getPassword();
 
-        if (!enkrypteretPassword.equals(enkrypteretBrugerPassword))
+        if (!hashedPassword.equals(hashedBrugerPassword))
             throw new ForkertPasswordException();
 
         brugere.remove(bruger);
 
         aktivBruger = null;
-    }
-
-    //TODO Opret TextHasher-klasse
-    public String enkrypterTekst(String tekst) {
-        String sha256hex = null;
-        try {
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            byte[] bytes = tekst.getBytes(StandardCharsets.UTF_8);
-            byte[] hash = digest.digest(bytes);
-            sha256hex = new String(Hex.encodeHex(hash));
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        return sha256hex;
+        // TODO Anders ville have Bruger til at klare hashing?
     }
 
     public Bruger getAktivBruger() {
@@ -70,9 +55,9 @@ public class BrugerManager {
 
     public boolean logInd(String email, String password) throws ForkertPasswordException {
         for (int i = 0; i < brugere.size(); i++) {
-            String enkrypteretPassword = enkrypterTekst(password);
+            String hashedPassword = tekstHasher.hashTekst(password);
             if (brugere.get(i).getEmail().equals(email)) {
-                if (brugere.get(i).getPassword().equals(enkrypteretPassword)){
+                if (brugere.get(i).getPassword().equals(hashedPassword)){
                     aktivBruger = brugere.get(i);
                     return true;
                 }
