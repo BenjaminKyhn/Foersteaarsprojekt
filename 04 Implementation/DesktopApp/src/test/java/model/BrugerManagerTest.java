fde6bdd23ff1a3c6b88d9434f80a6910a299fadb
Tommy Gultiano Hansen <54972963+EWCMF@users.noreply.github.com>
2020-06-01 11:33:29 +0200

    Implementerede UT010402 og ændrede navn på forrige test til det rigtige.

--- 04 Implementation/DesktopApp/src/test/java/model/BrugerManagerTest.java
+++ 04 Implementation/DesktopApp/src/test/java/model/BrugerManagerTest.java
@@ -5,9 +5,11 @@ import domain.Bruger;
 import model.exceptions.BrugerErIkkeBehandlerException;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 public class BrugerManagerTest {
     @Test
-    public void UT010402 () {
+    public void UT010401() {
         MockBruger mockBruger = new MockBruger("Johnny", "fas39luck", false);
         BrugerManager brugerManager = new TestbarBrugerManager();
         brugerManager.setAktivBruger(mockBruger);
@@ -15,8 +17,19 @@ public class BrugerManagerTest {
         String email = "hans@email.dk";
         String password = "123";
         assertThrows(BrugerErIkkeBehandlerException.class, () -> brugerManager.opretBruger(navn, email, password));
+    }
 
-
+    @Test
+    public void UT010402() throws BrugerErIkkeBehandlerException {
+        MockBruger mockBruger = new MockBruger("Johnny", "fas39luck", true);
+        BrugerManager brugerManager = new TestbarBrugerManager();
+        brugerManager.setAktivBruger(mockBruger);
+        String navn = "Hans";
+        String email = "hans@email.dk";
+        String password = "123";
+        brugerManager.opretBruger(navn, email, password);
+        String output = brugerManager.hentBrugere().get(0).getNavn();
+        assertEquals("Hans", output);
     }
 
     private class MockBruger extends Bruger {
@@ -49,13 +62,13 @@ public class BrugerManagerTest {
     private class TestbarBrugerManager extends BrugerManager {
         MockBruger aktivBruger;
 
+        public TestbarBrugerManager() {
+            setBrugere(new ArrayList<>());
+        }
+
         @Override
         public Bruger getAktivBruger() {
             return aktivBruger;
         }
-
-
-
     }
-
 }
