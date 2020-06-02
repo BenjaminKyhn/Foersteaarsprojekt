5ea6b3fd6e18d6da0e00bde7ac3bb35c1ba8c70d
Tommy Gultiano Hansen <54972963+EWCMF@users.noreply.github.com>
2020-06-02 12:08:40 +0200

    Fiksede test UT010103.

--- 04 Implementation/DesktopApp/src/test/java/usecases/BeskedManagerTest.java
+++ 04 Implementation/DesktopApp/src/test/java/usecases/BeskedManagerTest.java
@@ -22,11 +22,11 @@ public class BeskedManagerTest {
         assertThrows(BrugerFindesIkkeException.class, () -> beskedManager.opretChat("Egon", "Skulderskade"));
     }
 
-//    @Test
-//    public void opretChatUT010103() {
-//        BeskedManager beskedManager = new TestbarBeskedManager();
-//        assertThrows(BrugerFindesIkkeException.class, () -> beskedManager.opretChat(null, "Skulderskade"));
-//    }
+    @Test
+    public void opretChatUT010103() {
+        BeskedManager beskedManager = new TestbarBeskedManager();
+        assertThrows(BrugerFindesIkkeException.class, () -> beskedManager.opretChat(null, "Skulderskade"));
+    }
 
 
     private class MockBrugerManager extends BrugerManager {
@@ -37,6 +37,10 @@ public class BeskedManagerTest {
 
         @Override
         public Bruger hentBrugerMedNavn(String navn) {
+            if (navn == null) {
+                return null;
+            }
+
             if (!navn.equals("Boris")) {
                 return null;
             }
