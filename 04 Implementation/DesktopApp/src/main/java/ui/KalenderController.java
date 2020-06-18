56d057df8df6c8a38dc4fb5f198b72bd6d6f71c5
Benjamin Kyhn <benkyhn@hotmail.com>
2020-06-18 13:20:16 +0200

    Metoder til at fylde kalenderen med aftaler tilføjet

--- 04 Implementation/DesktopApp/src/main/java/ui/KalenderController.java
+++ 04 Implementation/DesktopApp/src/main/java/ui/KalenderController.java
@@ -1,12 +1,9 @@
 package ui;
 
-import com.calendarfx.model.Calendar;
+import com.calendarfx.model.*;
 import com.calendarfx.model.Calendar.Style;
-import com.calendarfx.model.CalendarSource;
 import com.calendarfx.util.CalendarFX;
 import com.calendarfx.view.CalendarView;
-import com.google.cloud.firestore.QuerySnapshot;
-import entities.Traeningsprogram;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
@@ -19,6 +16,10 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import model.BrugerFacade;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Year;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
@@ -35,7 +36,7 @@ public class KalenderController {
     @FXML
     private MenuBar menuBar;
 
-    public void initialize(){
+    public void initialize() {
         brugerFacade = BrugerFacade.getInstance();
 
         /** Sæt UI-elementer til at skalere med vinduets størrelse */
@@ -51,11 +52,34 @@ public class KalenderController {
     /**
      * Kalenderen tager 2-3 sekunder at indlæse, så derfor gøres det i en thread
      */
-    public void indlaesKalender(){
+    public void indlaesKalender() {
         Platform.runLater(() -> {
             CalendarView calendarView = new CalendarView();
             calenderViewHolder.getChildren().clear();
             calenderViewHolder.getChildren().add(calendarView);
+
+            Calendar ferie = new Calendar("Ferie");
+            ferie.setStyle(Style.STYLE1);
+            Calendar konsulationer = new Calendar("Konsulationer");
+            konsulationer.setStyle(Style.STYLE2);
+            Calendar behandlinger = new Calendar("Behandlinger");
+            behandlinger.setStyle(Style.STYLE3);
+            Calendar moeder = new Calendar("Møder");
+            moeder.setStyle(Style.STYLE4);
+            Calendar eksaminer = new Calendar("Eksaminer");
+            eksaminer.setStyle(Style.STYLE5);
+            Calendar eksamensforberedelse = new Calendar("Eksamensforberedelse");
+            eksamensforberedelse.setStyle(Style.STYLE6);
+
+            CalendarSource calendarSource = new CalendarSource("Aftaler");
+            calendarSource.getCalendars().addAll(ferie, konsulationer, behandlinger, moeder, eksaminer, eksamensforberedelse);
+
+            calendarView.getCalendarSources().setAll(calendarSource);
+
+            eksaminer.addEntry(tilfoejAftale("Førsteårseksamen", 2020, 6, 24, 10, 0,
+                    2020, 6, 24, 12, 0));
+            eksamensforberedelse.addEntry(tilfoejAftaleNu("Afprøv programmet"));
+            eksamensforberedelse.addEntry(tilfoejAftaleIDag("Kodning", 9, 0, 15, 0));
         });
     }
 
@@ -87,5 +111,38 @@ public class KalenderController {
 
         Stage stage = (Stage) kalenderAnchorPane.getScene().getWindow();
         stage.setScene(scene);
+    }
+
+    public Entry tilfoejAftale(String titel, int startAar, int startMaaned, int startDag, int startTime, int startMinut,
+                               int slutAaar, int slutMaaned, int slutDag, int slutTime, int slutMinut) {
+        LocalDate startDato = LocalDate.of(startAar, startMaaned, startDag);
+        LocalTime startTidspunkt = LocalTime.of(startTime, startMinut, 0);
+        LocalDate slutDato = LocalDate.of(slutAaar, slutMaaned, slutDag);
+        LocalTime slutTidspunkt = LocalTime.of(slutTime, slutMinut, 0);
+        Interval interval = new Interval(startDato, startTidspunkt, slutDato, slutTidspunkt);
+        return new Entry(titel, interval);
+        //TODO: Metoden skal flyttes til logik på et tidspunkt
+    }
+
+    public Entry tilfoejAftaleIDag(String titel, int startTime, int startMinut, int slutTime, int slutMinut) {
+        LocalDateTime nu = LocalDateTime.now();
+        LocalDate startDato = LocalDate.of(nu.getYear(), nu.getMonth(), nu.getDayOfMonth());
+        LocalTime startTidspunkt = LocalTime.of(startTime, startMinut, 0);
+        LocalDate slutDato = LocalDate.of(nu.getYear(), nu.getMonth(), nu.getDayOfMonth());
+        LocalTime slutTidspunkt = LocalTime.of(slutTime, slutMinut, 0);
+        Interval interval = new Interval(startDato, startTidspunkt, slutDato, slutTidspunkt);
+        return new Entry(titel, interval);
+        //TODO: Metoden skal flyttes til logik på et tidspunkt
+    }
+
+    public Entry tilfoejAftaleNu(String titel) {
+        LocalDateTime nu = LocalDateTime.now();
+        LocalDate startDato = LocalDate.of(nu.getYear(), nu.getMonth(), nu.getDayOfMonth());
+        LocalTime startTidspunkt = LocalTime.of(nu.getHour(), nu.getMinute(), nu.getSecond());
+        LocalDate slutDato = LocalDate.of(nu.getYear(), nu.getMonth(), nu.getDayOfMonth());
+        LocalTime slutTidspunkt = LocalTime.of(nu.getHour() + 1, nu.getMinute(), nu.getSecond());
+        Interval interval = new Interval(startDato, startTidspunkt, slutDato, slutTidspunkt);
+        return new Entry(titel, interval);
+        //TODO: Metoden skal flyttes til logik på et tidspunkt
     }
 }
